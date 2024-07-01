3894fd272d1c5c21b5a789f3bc4ee5fce4c9f70c
Mindy Albers <mindyjalbers@gmail.com>
2024-07-01 11:55:53 -0500

    Uploaded updated files
    This code was updated to catch errors from the user inputting incorrect keys, such as entering numbers instead of a word.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pandas
 
-# TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-
 phonetic_data = pandas.read_csv("nato_phonetic_alphabet.csv")
-phonetic_dict = {row.letter: row.code for (index, row) in phonetic_data.iterrows()}
-# print(nato_dict)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-phonetic_output_list = [phonetic_dict[letter] for letter in word]
-print(phonetic_output_list)
+phonetic_dict = {row.letter: row.code for (index, row) in phonetic_data.iterrows()}
+print(phonetic_dict)
+
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        phonetic_output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phonetic_output_list)
+
+
+generate_phonetic()
